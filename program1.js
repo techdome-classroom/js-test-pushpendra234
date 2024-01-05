2da6e4545ad5549876bc5cd783f69842d4dab603
pushpendra234 <94051456+pushpendra234@users.noreply.github.com>
2024-01-05 00:15:38 -0800

    Update program1.js

--- program1.js
+++ program1.js
@@ -2,10 +2,39 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
+var isValid = function(s)
+{
+    const stack=[];
+    const pairs={
+        '(':')',
+        '{':'}',
+        '[':']'
+    };
+    for(let i=0;i<s.length();i++)
+    {
+        const c=s[i];
+        if(c in pairs)
+        {
+            stack.push(c);
+        }
+        else{
+            const lastopened=stack.pop();
+            if(pairs[lastopened]!=c)
+            {
+                return false;
+            }
+        }
+    }
+    return stack.length()==0;
+
+
     
 };
 
 module.exports = { isValid };
+
+
+
+
 
 
