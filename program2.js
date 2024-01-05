86af5cf875d5145e5e99b57fc715e1ce37368444
pushpendra234 <94051456+pushpendra234@users.noreply.github.com>
2024-01-05 00:16:11 -0800

    Update program2.js

--- program2.js
+++ program2.js
@@ -3,8 +3,36 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    const romanvalues={
+        'I':1,
+        'V':5,
+        'X':10,
+        'L':50,
+        'C':100,
+        'D':500,
+        'M':1000
+    };
+    let res=0;
+    for(let i=0;i<s.length;i++)
+    {
+        const curr=romanvalues[s[i]];
+        const next=romanvalues[s[i+1]];
+        if(next&&curr<next)
+        {
+            res+=next-curr;
+            i++;
+        }
+        else
+        {
+            res+=curr;
+
+        }
+
+
+    }
+    return res;
     
 };
 
 
-module.exports={romanToInt}
+module.exports={romanToInt};
